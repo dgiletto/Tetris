6c7b33cc47546fc7c63859aa24339397e563ffcf
dgiletto <dgiletto@udel.edu>
2025-01-21 17:23:08 -0500

    Created Main

---
+++ Main.cpp
@@ -0,0 +1,91 @@
+#include "Game.h"
+#ifndef LINUX
+#include <windows.h>
+#endif
+
+/* Main */
+#ifndef LINUX
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main()
+#endif
+{
+    IO mIO;
+    int mScreenHeight = mIO.GetScreenHeight();
+    Pieces mPieces;
+    Board mBoard(&mPieces, mScreenHeight);
+    Game mGame(&mBoard, &mPieces, &mIO, mScreenHeight);
+    unsigned long mTime1 = SDL_GetTicks();
+
+    while (!mIO.IsKeyDown(SDLK_ESCAPE)) {
+        mIO.ClearScreen();
+        mGame.DrawScene();
+        mIO.UpdateScreen();
+
+        int mKey = mIO.PollKey();
+        switch(mKey) {
+            case (SDLK_RIGHT):
+            {
+                if (mBoard.IsPossibleMovement(mGame.mPosX + 1, mGame.mPosY, mGame.mPiece, mGame.mRotation)) {
+                    mGame.mPosX++;
+                }
+                break;
+            }
+            case (SDLK_LEFT):
+            {
+                if (mBoard.IsPossibleMovement(mGame.mPosX - 1, mGame.mPosY, mGame.mPiece, mGame.mRotation)) {
+                    mGame.mPosX--;
+                }
+                break;
+            }
+            case (SDLK_DOWN):
+            {
+                if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation)) {
+                    mGame.mPosY++;
+                }
+                break;
+            }
+            case (SDLK_x):
+            {
+                while(mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation)) {
+                    mGame.mPosY++;
+                }
+                mBoard.StorePiece(mGame.mPosX, mGame.mPosY - 1, mGame.mPiece, mGame.mRotation);
+                mBoard.DeletePossibleLines();
+                if (mBoard.IsGameOver()) {
+                    mIO.GetKey();
+                    exit(0);
+                }
+
+                mGame.CreateNewPiece();
+                break;
+            }
+            case (SDLK_z):
+            {
+                if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, (mGame.mRotation + 1) % 4)) {
+                    mGame.mRotation = (mGame.mRotation + 1) % 4;
+                }
+                break;
+            }
+        }
+
+        unsigned long mTime2 = SDL_GetTicks();
+        if ((mTime2 - mTime1) > WAIT_TIME) {
+            if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation)) {
+                mGame.mPosY++;
+            }
+            else {
+                mBoard.StorePiece(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation);
+                mBoard.DeletePossibleLines();
+                if (mBoard.IsGameOver()) {
+                    mIO.GetKey();
+                    exit(0);
+                }
+
+                mGame.CreateNewPiece();
+            }
+            mTime1 = SDL_GetTicks();
+        }
+    }
+    return 0;
+}
